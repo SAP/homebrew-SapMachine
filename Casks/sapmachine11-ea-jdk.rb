4524ff3e4455bffff5b3dc8b4ab4d127aabf2897
SapMachine <sapmachine@sap.com>
2019-05-21 05:05:03 +0200

    Updated sapmachine11-ea-jdk.rb.

--- Casks/sapmachine11-ea-jdk.rb
+++ Casks/sapmachine11-ea-jdk.rb
@@ -1,13 +1,14 @@
+
 cask 'sapmachine11-ea-jdk' do
-  version '11.0.4,2'
-  sha256 'bf6bc12790fca78f7f0085fefbb715e8db3ddfb5236a78e8bfb366f9e8209d1d'
+  version '11.0.4,3'
+  sha256 'f14c632047b37a855eb4f7b1618a68421696e7dc734650caee0b49df3f408977'
 
   url "https://github.com/SAP/SapMachine/releases/download/sapmachine-#{version.before_comma}%2B#{version.after_comma}/sapmachine-jdk-#{version.before_comma}-ea.#{version.after_comma}_osx-x64_bin.tar.gz"
   appcast "https://sap.github.io/SapMachine/latest/#{version.major}"
   name 'SapMachine OpenJDK Development Kit'
   homepage 'https://sapmachine.io/'
 
-  artifact "sapmachine-jdk-#{version.before_comma}.jdk", target: "/Library/Java/JavaVirtualMachines/sapmachine-11-ea.jdk"
+  artifact "sapmachine-jdk-#{version.before_comma}.jdk", target: "/Library/Java/JavaVirtualMachines/sapmachine-#{version.major}-ea.jdk"
 
   uninstall rmdir: '/Library/Java/JavaVirtualMachines'
 end
