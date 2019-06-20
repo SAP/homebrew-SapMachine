0517120c49aca4122a4752edba4abeb25f969dfc
SapMachine <sapmachine@sap.com>
2019-06-20 05:00:17 +0200

    Updated sapmachine14-ea-jdk.rb.

---
+++ Casks/sapmachine14-ea-jdk.rb
@@ -0,0 +1,14 @@
+
+cask 'sapmachine14-ea-jdk' do
+  version '14,1'
+  sha256 '1c5999af940ef1247c745cbd9a3d1bf2fecf6c8c067f1b09111a62eae12bdd01'
+
+  url "https://github.com/SAP/SapMachine/releases/download/sapmachine-#{version.before_comma}%2B#{version.after_comma}/sapmachine-jdk-#{version.before_comma}-ea.#{version.after_comma}_osx-x64_bin.tar.gz"
+  appcast "https://sap.github.io/SapMachine/latest/#{version.major}"
+  name 'SapMachine OpenJDK Development Kit'
+  homepage 'https://sapmachine.io/'
+
+  artifact "sapmachine-jdk-#{version.before_comma}.jdk", target: "/Library/Java/JavaVirtualMachines/sapmachine-#{version.major}-ea.jdk"
+
+  uninstall rmdir: '/Library/Java/JavaVirtualMachines'
+end
