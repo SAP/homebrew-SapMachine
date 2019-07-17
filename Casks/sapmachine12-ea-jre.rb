7bcb78b6f2c8b12fb9d1738788108a175b0dfa5d
SapMachine <sapmachine@sap.com>
2019-07-17 18:06:43 +0200

    Updated sapmachine12-ea-jre.rb.

--- Casks/sapmachine12-ea-jre.rb
+++ Casks/sapmachine12-ea-jre.rb
@@ -1,13 +1,14 @@
+
 cask 'sapmachine12-ea-jre' do
-  version '12,33'
-  sha256 'baa9f05594d1530bc85183d618b65d4ef2417df678e5f9bf22292a165d72e694'
+  version '12.0.2,None'
+  sha256 '7480e85d58fe794972a185f638a71f760dbb00172776ec5175dade9943dcb5cd'
 
   url "https://github.com/SAP/SapMachine/releases/download/sapmachine-#{version.before_comma}%2B#{version.after_comma}/sapmachine-jre-#{version.before_comma}-ea.#{version.after_comma}_osx-x64_bin.tar.gz"
   appcast "https://sap.github.io/SapMachine/latest/#{version.major}"
   name 'SapMachine OpenJDK Development Kit'
   homepage 'https://sapmachine.io/'
 
-  artifact "sapmachine-jre-#{version.before_comma}.jre", target: "/Library/Java/JavaVirtualMachines/sapmachine-12-ea.jre"
+  artifact "sapmachine-jre-#{version.before_comma}.jre", target: "/Library/Java/JavaVirtualMachines/sapmachine-#{version.major}-ea.jre"
 
   uninstall rmdir: '/Library/Java/JavaVirtualMachines'
 end
