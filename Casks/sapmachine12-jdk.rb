834737be31b5f6adcc3611fefe5316a1f99b0a6b
SapMachine <sapmachine@sap.com>
2019-07-17 22:27:52 +0200

    Updated sapmachine12-jdk.rb.

--- Casks/sapmachine12-jdk.rb
+++ Casks/sapmachine12-jdk.rb
@@ -1,13 +1,14 @@
+
 cask 'sapmachine12-jdk' do
-  version '12.0.1'
-  sha256 '5094b7437356c00a8a347f8c178b10b0228bdb840248980ca28008b746a7fc05'
+  version '12.0.2'
+  sha256 'ede180009ded8bef782e41ad72bd242faa6749ac889297c01777559278fc4848'
 
   url "https://github.com/SAP/SapMachine/releases/download/sapmachine-#{version}/sapmachine-jdk-#{version}_osx-x64_bin.tar.gz"
   appcast "https://sap.github.io/SapMachine/latest/#{version.major}"
   name 'SapMachine OpenJDK Development Kit'
   homepage 'https://sapmachine.io/'
 
-  artifact "sapmachine-jdk-#{version}.jdk", target: "/Library/Java/JavaVirtualMachines/sapmachine-12.jdk"
+  artifact "sapmachine-jdk-#{version}.jdk", target: "/Library/Java/JavaVirtualMachines/sapmachine-#{version.major}.jdk"
 
   uninstall rmdir: '/Library/Java/JavaVirtualMachines'
 end
