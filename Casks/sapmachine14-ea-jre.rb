000518f953e48814afbad48bc041745a921ef7d6
SapMachine <sapmachine@sap.com>
2019-09-09 12:09:58 +0200

    Updated sapmachine14-ea-jre.rb.

--- Casks/sapmachine14-ea-jre.rb
+++ Casks/sapmachine14-ea-jre.rb
@@ -1,9 +1,9 @@
 
 cask 'sapmachine14-ea-jre' do
-  version '14,12'
-  sha256 '99c61ec957e8d0978ef105390e2d4c61da89ca49bd54d9797e869b3eef0d9d9f'
+  version '14,13'
+  sha256 '896346f2ceb93f14a428df4eb36d28ce1c1a28ad3cae559347c7094c64c71478'
 
-  url "https://github.com/SAP/SapMachine/releases/download/sapmachine-#{version.before_comma}%2B#{version.after_comma}/sapmachine-jre-#{version.before_comma}-ea.#{version.after_comma}_osx-x64_bin.tar.gz"
+  url "https://github.com/SAP/SapMachine/releases/download/sapmachine-#{version.before_comma}%2B#{version.after_comma}/sapmachine-jre-#{version.before_comma}-ea.#{version.after_comma}_osx-x64_bin.dmg"
   appcast "https://sap.github.io/SapMachine/latest/#{version.major}"
   name 'SapMachine OpenJDK Development Kit'
   homepage 'https://sapmachine.io/'
