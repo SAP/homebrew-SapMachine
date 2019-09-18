4757a9fa4825711060fcb180a40b1a3650c25f02
SapMachine <sapmachine@sap.com>
2019-09-18 11:03:34 +0200

    Updated sapmachine13-jdk.rb.

---
+++ Casks/sapmachine13-jdk.rb
@@ -0,0 +1,14 @@
+
+cask 'sapmachine13-jdk' do
+  version '13'
+  sha256 'd4f85394cb02db999baa906dedf51897b7b99189cd4c44f298c305529ee0cb2a'
+
+  url "https://github.com/SAP/SapMachine/releases/download/sapmachine-#{version}/sapmachine-jdk-#{version}_osx-x64_bin.dmg"
+  appcast "https://sap.github.io/SapMachine/latest/#{version.major}"
+  name 'SapMachine OpenJDK Development Kit'
+  homepage 'https://sapmachine.io/'
+
+  artifact "sapmachine-jdk-#{version}.jdk", target: "/Library/Java/JavaVirtualMachines/sapmachine-#{version.major}.jdk"
+
+  uninstall rmdir: '/Library/Java/JavaVirtualMachines'
+end
