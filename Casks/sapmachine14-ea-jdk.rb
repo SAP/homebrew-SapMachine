87cf871dc0aafa3938d3f02bb0b231b8d83e760b
SapMachine <sapmachine@sap.com>
2019-12-03 20:51:26 +0100

    Updated sapmachine14-ea-jdk.rb.

--- Casks/sapmachine14-ea-jdk.rb
+++ Casks/sapmachine14-ea-jdk.rb
@@ -1,7 +1,7 @@
 
 cask 'sapmachine14-ea-jdk' do
-  version '14,24'
-  sha256 '384726608a529acd0bca63ca2acee6c39338fa03b2bed05a2927ff3e527306db'
+  version '14,25'
+  sha256 '46d112e9171f1e87d1aed752e39e85e44c0e8298c339b8b00dfb64d96b70ab31'
 
   url "https://github.com/SAP/SapMachine/releases/download/sapmachine-#{version.before_comma}%2B#{version.after_comma}/sapmachine-jdk-#{version.before_comma}-ea.#{version.after_comma}_osx-x64_bin.dmg"
   appcast "https://sap.github.io/SapMachine/latest/#{version.major}"
