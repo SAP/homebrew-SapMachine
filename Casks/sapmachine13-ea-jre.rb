50983948b13613b7d667ccbc4ed62c53f4f62319
SapMachine <sapmachine@sap.com>
2019-12-30 16:51:27 +0100

    Updated sapmachine13-ea-jre.rb.

--- Casks/sapmachine13-ea-jre.rb
+++ Casks/sapmachine13-ea-jre.rb
@@ -1,7 +1,7 @@
 
 cask 'sapmachine13-ea-jre' do
-  version '13.0.2,1'
-  sha256 '114807533388a329aa611272f0928c2dc36340f7cc1e905aa17fe381227fd02e'
+  version '13.0.2,2'
+  sha256 '6a93b2575c4b4b081a0e14d320b087c31a83e6ca93bea7695761e796d3ed9bd6'
 
   url "https://github.com/SAP/SapMachine/releases/download/sapmachine-#{version.before_comma}%2B#{version.after_comma}/sapmachine-jre-#{version.before_comma}-ea.#{version.after_comma}_osx-x64_bin.dmg"
   appcast "https://sap.github.io/SapMachine/latest/#{version.major}"
