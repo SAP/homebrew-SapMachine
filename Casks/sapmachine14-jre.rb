ddc1998dfb87c27ec1aedff59a55e4ebef0def2d
SapMachine <sapmachine@sap.com>
2020-03-19 15:39:23 +0100

    Updated sapmachine14-jre.rb.

---
+++ Casks/sapmachine14-jre.rb
@@ -0,0 +1,14 @@
+
+cask 'sapmachine14-jre' do
+  version '14'
+  sha256 '6fcfc20a29fe9dda8ef5ebad7791bc468087dee1426d8ecfd3443a1e8c05c271'
+
+  url "https://github.com/SAP/SapMachine/releases/download/sapmachine-#{version}/sapmachine-jre-#{version}_osx-x64_bin.dmg"
+  appcast "https://sap.github.io/SapMachine/latest/#{version.major}"
+  name 'SapMachine OpenJDK Development Kit'
+  homepage 'https://sapmachine.io/'
+
+  artifact "sapmachine-jre-#{version}.jre", target: "/Library/Java/JavaVirtualMachines/sapmachine-#{version.major}.jre"
+
+  uninstall rmdir: '/Library/Java/JavaVirtualMachines'
+end
