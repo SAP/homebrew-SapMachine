821ca58159409f5dc84a457fb4d2d0f174b6bf38
SapMachine <sapmachine@sap.com>
2020-06-06 07:02:48 +0200

    Updated sapmachine15-ea-jre.rb.

--- Casks/sapmachine15-ea-jre.rb
+++ Casks/sapmachine15-ea-jre.rb
@@ -1,7 +1,7 @@
 
 cask 'sapmachine15-ea-jre' do
-  version '15,25'
-  sha256 '15a4304bb6af95f6cba4c14a0fddf6060eb1dcc108fbc56253fce79ad9717f13'
+  version '15,26'
+  sha256 '09f32c115995355b0ffa6dbb76af71f6733bf319628213fcecbd59b160a2c975'
 
   url "https://github.com/SAP/SapMachine/releases/download/sapmachine-#{version.before_comma}%2B#{version.after_comma}/sapmachine-jre-#{version.before_comma}-ea.#{version.after_comma}_osx-x64_bin.dmg"
   appcast "https://sap.github.io/SapMachine/latest/#{version.major}"
