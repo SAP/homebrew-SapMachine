ca03de9b5e0ab843ea6038b8bcf2ecb6a1592616
SapMachine <sapmachine@sap.com>
2020-06-18 05:12:21 +0200

    Updated sapmachine16-ea-jdk.rb.

---
+++ Casks/sapmachine16-ea-jdk.rb
@@ -0,0 +1,14 @@
+
+cask 'sapmachine16-ea-jdk' do
+  version '16,1'
+  sha256 'db730f1d65b7877cc0fbb0fb663257163862f80f0dc4bbb5a71005e66ff600c8'
+
+  url "https://github.com/SAP/SapMachine/releases/download/sapmachine-#{version.before_comma}%2B#{version.after_comma}/sapmachine-jdk-#{version.before_comma}-ea.#{version.after_comma}_osx-x64_bin.dmg"
+  appcast "https://sap.github.io/SapMachine/latest/#{version.major}"
+  name 'SapMachine OpenJDK Development Kit'
+  homepage 'https://sapmachine.io/'
+
+  artifact "sapmachine-jdk-#{version.before_comma}.jdk", target: "/Library/Java/JavaVirtualMachines/sapmachine-#{version.major}-ea.jdk"
+
+  uninstall rmdir: '/Library/Java/JavaVirtualMachines'
+end
