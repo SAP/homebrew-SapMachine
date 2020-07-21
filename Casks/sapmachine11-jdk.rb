fe66e7e75c13bacf98e472e2ada97870d743981d
René Schünemann <rene.schuenemann@gmail.com>
2020-07-21 10:21:04 +0200

    Fixed sha256 sum of 11.0.8 jdk.

--- Casks/sapmachine11-jdk.rb
+++ Casks/sapmachine11-jdk.rb
@@ -1,7 +1,7 @@
 
 cask 'sapmachine11-jdk' do
   version '11.0.8'
-  sha256 '6935a166231deb6357e83cf6b0be214b06d05a73415bfda45f367c0eec9edc55'
+  sha256 '15829218eb47303b74eaecbc28d307c10123d841e67fe9701a904cd45365dc2d'
 
   url "https://github.com/SAP/SapMachine/releases/download/sapmachine-#{version}/sapmachine-jdk-#{version}_osx-x64_bin.dmg"
   appcast "https://sap.github.io/SapMachine/latest/#{version.major}"
