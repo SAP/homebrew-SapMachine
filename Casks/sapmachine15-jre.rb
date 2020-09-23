8dd3e1cf5b7a81131f12793d8a4258d7d92141cb
SapMachine <sapmachine@sap.com>
2020-09-23 14:22:53 +0200

    Updated sapmachine15-jre.rb.

---
+++ Casks/sapmachine15-jre.rb
@@ -0,0 +1,14 @@
+
+cask 'sapmachine15-jre' do
+  version '15'
+  sha256 'd9e840e6fb2f5088a236d66cd4acd1e0ebf37d446dcb371b0ed187d20e5ace59'
+
+  url "https://github.com/SAP/SapMachine/releases/download/sapmachine-#{version}/sapmachine-jre-#{version}_osx-x64_bin.dmg"
+  appcast "https://sap.github.io/SapMachine/latest/#{version.major}"
+  name 'SapMachine OpenJDK Development Kit'
+  homepage 'https://sapmachine.io/'
+
+  artifact "sapmachine-jre-#{version}.jre", target: "/Library/Java/JavaVirtualMachines/sapmachine-#{version.major}.jre"
+
+  uninstall rmdir: '/Library/Java/JavaVirtualMachines'
+end
