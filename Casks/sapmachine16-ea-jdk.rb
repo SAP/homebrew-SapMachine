6d666be046ad5d9af2da992825a5aa9bde4f37dd
SapMachine <sapmachine@sap.com>
2020-11-14 13:05:40 +0100

    Update sapmachine16-ea-jdk.rb (16+24).

--- Casks/sapmachine16-ea-jdk.rb
+++ Casks/sapmachine16-ea-jdk.rb
@@ -1,7 +1,7 @@
 
 cask 'sapmachine16-ea-jdk' do
-  version '16,23'
-  sha256 '4eff672c89359512a0f293b844056d915106efaa40113f12c7ffdc25ddf6a443'
+  version '16,24'
+  sha256 '9c21afa67364523fad73d61c59ac159ac43933d933d433e3e70a16222b79ee71'
 
   url "https://github.com/SAP/SapMachine/releases/download/sapmachine-#{version.before_comma}%2B#{version.after_comma}/sapmachine-jdk-#{version.before_comma}-ea.#{version.after_comma}_osx-x64_bin.dmg"
   appcast "https://sap.github.io/SapMachine/latest/#{version.major}"
