9a266b6d3d20a3ef7724c8e25b064bbb15399e40
Christoph Langer <christoph.langer@sap.com>
2020-12-11 15:12:10 +0100

    Fix download location for 17+0 ea jdk

--- Casks/sapmachine17-ea-jdk.rb
+++ Casks/sapmachine17-ea-jdk.rb
@@ -3,7 +3,7 @@ cask 'sapmachine17-ea-jdk' do
   version '17,0'
   sha256 '455b495df5ea273c06816cb7fcedd6509875a58e6876b820874cf3c2ab4dbd5e'
 
-  url "https://github.com/SAP/SapMachine/releases/download/sapmachine-#{version.before_comma}%2B#{version.after_comma}/sapmachine-jdk-#{version.before_comma}-ea.#{version.after_comma}_osx-x64_bin.dmg"
+  url "https://github.com/SAP/SapMachine/releases/download/sapmachine-#{version.before_comma}%2B#{version.after_comma}/sapmachine-jdk-16-ea.#{version.after_comma}_osx-x64_bin.dmg"
   appcast "https://sap.github.io/SapMachine/latest/#{version.major}"
   name 'SapMachine OpenJDK Development Kit'
   homepage 'https://sapmachine.io/'
