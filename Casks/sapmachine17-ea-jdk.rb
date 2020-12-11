4752314df80af5e02bf6e9880dd05d70f20b81c6
SapMachine <sapmachine@sap.com>
2020-12-11 15:00:01 +0100

    Update sapmachine17-ea-jdk.rb (17+0).

---
+++ Casks/sapmachine17-ea-jdk.rb
@@ -0,0 +1,14 @@
+
+cask 'sapmachine17-ea-jdk' do
+  version '17,0'
+  sha256 '455b495df5ea273c06816cb7fcedd6509875a58e6876b820874cf3c2ab4dbd5e'
+
+  url "https://github.com/SAP/SapMachine/releases/download/sapmachine-#{version.before_comma}%2B#{version.after_comma}/sapmachine-jdk-#{version.before_comma}-ea.#{version.after_comma}_osx-x64_bin.dmg"
+  appcast "https://sap.github.io/SapMachine/latest/#{version.major}"
+  name 'SapMachine OpenJDK Development Kit'
+  homepage 'https://sapmachine.io/'
+
+  artifact "sapmachine-jdk-#{version.before_comma}.jdk", target: "/Library/Java/JavaVirtualMachines/sapmachine-#{version.major}-ea.jdk"
+
+  uninstall rmdir: '/Library/Java/JavaVirtualMachines'
+end
