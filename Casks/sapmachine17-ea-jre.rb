dd9cfb1e9abf9763c50258c93e5d36d3b2a86b71
Christoph Langer <christoph.langer@sap.com>
2021-05-13 23:24:50 +0200

    Fix sapmachine17-ea-jre cask after renaming bundle file with build 21

--- Casks/sapmachine17-ea-jre.rb
+++ Casks/sapmachine17-ea-jre.rb
@@ -3,7 +3,7 @@ cask 'sapmachine17-ea-jre' do
   version '17,21'
   sha256 '5cd50cfde9eaf283324e9c185de69167424ac23d7fcb42251e70bc315984ab44'
 
-  url "https://github.com/SAP/SapMachine/releases/download/sapmachine-#{version.before_comma}%2B#{version.after_comma}/sapmachine-jre-#{version.before_comma}-ea.#{version.after_comma}_osx-x64_bin.dmg"
+  url "https://github.com/SAP/SapMachine/releases/download/sapmachine-#{version.before_comma}%2B#{version.after_comma}/sapmachine-jre-#{version.before_comma}-ea.#{version.after_comma}_macos-x64_bin.dmg"
   appcast "https://sap.github.io/SapMachine/latest/#{version.major}"
   name 'SapMachine OpenJDK Development Kit'
   homepage 'https://sapmachine.io/'
