fe9815b858b52e079269be52c4df49cbf2f531fc
SapMachine <sapmachine@sap.com>
2021-06-21 10:41:57 +0200

    Update sapmachine18-ea-jre.rb (18+1).

---
+++ Casks/sapmachine18-ea-jre.rb
@@ -0,0 +1,14 @@
+
+cask 'sapmachine18-ea-jre' do
+  version '18,1'
+  sha256 '2d1ccff7f4b3d215c3cf2caad3d8ce1af77dee33abb685b3ec1f5768c993fff9'
+
+  url "https://github.com/SAP/SapMachine/releases/download/sapmachine-#{version.before_comma}%2B#{version.after_comma}/sapmachine-jre-#{version.before_comma}-ea.#{version.after_comma}_macos-x64_bin.dmg"
+  appcast "https://sap.github.io/SapMachine/latest/#{version.major}"
+  name 'SapMachine OpenJDK Development Kit'
+  homepage 'https://sapmachine.io/'
+
+  artifact "sapmachine-jre-#{version.before_comma}.jre", target: "/Library/Java/JavaVirtualMachines/sapmachine-#{version.major}-ea.jre"
+
+  uninstall rmdir: '/Library/Java/JavaVirtualMachines'
+end
