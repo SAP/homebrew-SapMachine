7f719c5bcdc7a7494bfec826012169458493d33a
SapMachine <sapmachine@sap.com>
2021-09-14 13:55:16 +0200

    Update sapmachine17-jdk.rb (17).

---
+++ Casks/sapmachine17-jdk.rb
@@ -0,0 +1,20 @@
+
+cask 'sapmachine17-jdk' do
+  version '17'
+
+  if Hardware::CPU.intel?
+    url "https://github.com/SAP/SapMachine/releases/download/sapmachine-#{version}/sapmachine-jdk-#{version}_macos-x64_bin.dmg"
+    sha256 '0b29168998bf0a72107aca7eff5a1d80a9dacbe5af5cbdbf6026a746ff093dfe'
+  else
+    url "https://github.com/SAP/SapMachine/releases/download/sapmachine-#{version}/sapmachine-jdk-#{version}_macos-aarch64_bin.dmg"
+    sha256 'e993513eb8837399507009ebbc8f980de6fd57ce55196a2e102bfd7fb617530f'
+  end
+
+  appcast "https://sap.github.io/SapMachine/latest/#{version.major}"
+  name 'SapMachine OpenJDK Development Kit'
+  homepage 'https://sapmachine.io/'
+
+  artifact "sapmachine-jdk-#{version}.jdk", target: "/Library/Java/JavaVirtualMachines/sapmachine-#{version.major}.jdk"
+
+  uninstall rmdir: '/Library/Java/JavaVirtualMachines'
+end
