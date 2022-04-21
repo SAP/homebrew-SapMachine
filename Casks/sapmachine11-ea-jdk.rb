1e0f7e70168081b5a54cf2e3b45a9bcbb9de68e5
SapMachine <sapmachine@sap.com>
2022-04-21 12:01:47 +0200

    Update sapmachine11-ea-jdk.rb (11.0.15+9).

--- Casks/sapmachine11-ea-jdk.rb
+++ Casks/sapmachine11-ea-jdk.rb
@@ -1,7 +1,7 @@
 
 cask 'sapmachine11-ea-jdk' do
-  version '11.0.15,8'
-  sha256 '0e226833045731b52b08dbc93d4e894107e32ec2198275eb53fee8e7d64478df'
+  version '11.0.15,9'
+  sha256 'efd0f29b79db8d83c621464c52852942b537ca66293d33a770733d4ae6671846'
 
   url "https://github.com/SAP/SapMachine/releases/download/sapmachine-#{version.before_comma}%2B#{version.after_comma}/sapmachine-jdk-#{version.before_comma}-ea.#{version.after_comma}_osx-x64_bin.dmg"
   appcast "https://sap.github.io/SapMachine/latest/#{version.major}"
