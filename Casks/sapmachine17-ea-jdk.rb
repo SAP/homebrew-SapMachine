d15c22413125f7bed909bc91b8d35de42f1d34e8
SapMachine <sapmachine@sap.com>
2022-05-31 11:07:59 +0200

    Update sapmachine17-ea-jdk.rb (17.0.4+4).

--- Casks/sapmachine17-ea-jdk.rb
+++ Casks/sapmachine17-ea-jdk.rb
@@ -1,20 +1,23 @@
 
-cask 'sapmachine17-ea-jdk' do
-  version '17.0.4,4'
+cask "sapmachine17-ea-jdk" do
+  version "17.0.4,4"
 
   if Hardware::CPU.intel?
-    url "https://github.com/SAP/SapMachine/releases/download/sapmachine-#{version.before_comma}%2B#{version.after_comma}/sapmachine-jdk-#{version.before_comma}-ea.#{version.after_comma}_macos-x64_bin.dmg"
-    sha256 '9b40bd24bd371d86880a9fc136b724aa0aedf298176d042b00521e294a01b310'
+    url "https://github.com/SAP/SapMachine/releases/download/sapmachine-#{version.before_comma}%2B#{version.after_comma}/sapmachine-jdk-#{version.before_comma}-ea.#{version.after_comma}_macos-x64_bin.dmg",
+         verified: "https://github.com/SAP/SapMachine"
+    sha256 "9b40bd24bd371d86880a9fc136b724aa0aedf298176d042b00521e294a01b310"
   else
-    url "https://github.com/SAP/SapMachine/releases/download/sapmachine-#{version.before_comma}%2B#{version.after_comma}/sapmachine-jdk-#{version.before_comma}-ea.#{version.after_comma}_macos-aarch64_bin.dmg"
-    sha256 '1ad6dbe0314db0f9cd111ee7bce2c8624e0fbc42a40a680f01f5249d3cae007f'
+    url "https://github.com/SAP/SapMachine/releases/download/sapmachine-#{version.before_comma}%2B#{version.after_comma}/sapmachine-jdk-#{version.before_comma}-ea.#{version.after_comma}_macos-aarch64_bin.dmg",
+         verified: "https://github.com/SAP/SapMachine"
+    sha256 "1ad6dbe0314db0f9cd111ee7bce2c8624e0fbc42a40a680f01f5249d3cae007f"
   end
 
   appcast "https://sap.github.io/SapMachine/latest/#{version.major}"
-  name 'SapMachine OpenJDK Development Kit'
-  homepage 'https://sapmachine.io/'
+  name "SapMachine OpenJDK Development Kit"
+  desc "OpenJDK build from SAP"
+  homepage "https://sapmachine.io/"
 
   artifact "sapmachine-jdk-#{version.before_comma}.jdk", target: "/Library/Java/JavaVirtualMachines/sapmachine-#{version.major}-ea.jdk"
 
-  uninstall rmdir: '/Library/Java/JavaVirtualMachines'
+  uninstall rmdir: "/Library/Java/JavaVirtualMachines"
 end
