dc1decfb0c7d1526b340d299b4fbde6456dea8f2
SapMachine <sapmachine@sap.com>
2022-05-31 14:27:59 +0200

    Update sapmachine18-jre.rb (18.0.1.1).

--- Casks/sapmachine18-jre.rb
+++ Casks/sapmachine18-jre.rb
@@ -1,20 +1,23 @@
 
-cask 'sapmachine18-jre' do
-  version '18.0.1.1'
+cask "sapmachine18-jre" do
+  version "18.0.1.1"
 
   if Hardware::CPU.intel?
-    url "https://github.com/SAP/SapMachine/releases/download/sapmachine-#{version}/sapmachine-jre-#{version}_macos-x64_bin.dmg"
-    sha256 'a786f86029ea90f0e2d74bc1fc5096e63813f90fbc8839e6b248f457fb3b8177'
+    url "https://github.com/SAP/SapMachine/releases/download/sapmachine-#{version}/sapmachine-jre-#{version}_macos-x64_bin.dmg",
+         verified: "https://github.com/SAP/SapMachine"
+    sha256 "a786f86029ea90f0e2d74bc1fc5096e63813f90fbc8839e6b248f457fb3b8177"
   else
-    url "https://github.com/SAP/SapMachine/releases/download/sapmachine-#{version}/sapmachine-jre-#{version}_macos-aarch64_bin.dmg"
-    sha256 '1779b501f404c106a28bb4873c08207c750cac659d2bf44df49d955f1ecb7fe1'
+    url "https://github.com/SAP/SapMachine/releases/download/sapmachine-#{version}/sapmachine-jre-#{version}_macos-aarch64_bin.dmg",
+         verified: "https://github.com/SAP/SapMachine"
+    sha256 "1779b501f404c106a28bb4873c08207c750cac659d2bf44df49d955f1ecb7fe1"
   end
 
   appcast "https://sap.github.io/SapMachine/latest/#{version.major}"
-  name 'SapMachine OpenJDK Development Kit'
-  homepage 'https://sapmachine.io/'
+  name "SapMachine OpenJDK Development Kit"
+  desc "OpenJDK build from SAP"
+  homepage "https://sapmachine.io/"
 
   artifact "sapmachine-jre-#{version}.jre", target: "/Library/Java/JavaVirtualMachines/sapmachine-#{version.major}.jre"
 
-  uninstall rmdir: '/Library/Java/JavaVirtualMachines'
+  uninstall rmdir: "/Library/Java/JavaVirtualMachines"
 end
