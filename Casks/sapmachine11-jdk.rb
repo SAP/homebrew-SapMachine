f5050e69d5ecd323527a5465ababd607c6b6fa5f
SapMachine <sapmachine@sap.com>
2022-07-20 11:05:28 +0200

    Update sapmachine11-jdk.rb (11.0.16).

--- Casks/sapmachine11-jdk.rb
+++ Casks/sapmachine11-jdk.rb
@@ -1,10 +1,17 @@
 
 cask "sapmachine11-jdk" do
-  version "11.0.15.0.1"
-  sha256 "3f61d992efc8bf53d56338a4b1ae0e00ff5ee3ef8c449a2c563de81cbbf0b336"
+  version "11.0.16"
 
-  url "https://github.com/SAP/SapMachine/releases/download/sapmachine-#{version}/sapmachine-jdk-#{version}_osx-x64_bin.dmg",
-       verified: "https://github.com/SAP/SapMachine"
+  if Hardware::CPU.intel?
+    url "https://github.com/SAP/SapMachine/releases/download/sapmachine-#{version}/sapmachine-jdk-#{version}_macos-x64_bin.dmg",
+         verified: "https://github.com/SAP/SapMachine"
+    sha256 "1204b77c9f61da2597fd7a24bd5906a135b6308ed6809d9e077be544aa099a72"
+  else
+    url "https://github.com/SAP/SapMachine/releases/download/sapmachine-#{version}/sapmachine-jdk-#{version}_macos-aarch64_bin.dmg",
+         verified: "https://github.com/SAP/SapMachine"
+    sha256 "121ea9d9ed03aa1457db61e5ee5bd9cc011bca61f726bd96449188ae4c41892c"
+  end
+
   appcast "https://sap.github.io/SapMachine/latest/#{version.major}"
   name "SapMachine OpenJDK Development Kit"
   desc "OpenJDK build from SAP"
