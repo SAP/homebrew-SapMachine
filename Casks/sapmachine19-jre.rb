ff95f9472b922ae742f69b22ad28bd9cc0f3d030
SapMachine <sapmachine@sap.com>
2022-09-20 13:34:02 +0200

    Update sapmachine19-jre.rb (19).

---
+++ Casks/sapmachine19-jre.rb
@@ -0,0 +1,23 @@
+
+cask "sapmachine19-jre" do
+  version "19"
+
+  if Hardware::CPU.intel?
+    url "https://github.com/SAP/SapMachine/releases/download/sapmachine-#{version}/sapmachine-jre-#{version}_macos-x64_bin.dmg",
+         verified: "https://github.com/SAP/SapMachine"
+    sha256 "d6959a17ff7fd0825a6e482d3628e3bdf3bc8d4f7edcb23023fa594813b1c53f"
+  else
+    url "https://github.com/SAP/SapMachine/releases/download/sapmachine-#{version}/sapmachine-jre-#{version}_macos-aarch64_bin.dmg",
+         verified: "https://github.com/SAP/SapMachine"
+    sha256 "8f724d831ac5f76c8a6eddf693851e39b792460801c074c6670fb12a08c5be83"
+  end
+
+  appcast "https://sap.github.io/SapMachine/latest/#{version.major}"
+  name "SapMachine OpenJDK Development Kit"
+  desc "OpenJDK build from SAP"
+  homepage "https://sapmachine.io/"
+
+  artifact "sapmachine-jre-#{version}.jre", target: "/Library/Java/JavaVirtualMachines/sapmachine-#{version.major}.jre"
+
+  uninstall rmdir: "/Library/Java/JavaVirtualMachines"
+end
