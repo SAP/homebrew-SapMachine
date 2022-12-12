f588cc3238cf96825a63708aea0d9fcebfff15dc
SapMachine <sapmachine@sap.com>
2022-12-12 09:37:54 +0100

    Update sapmachine21-ea-jdk.rb (21+1).

---
+++ Casks/sapmachine21-ea-jdk.rb
@@ -0,0 +1,23 @@
+
+cask "sapmachine21-ea-jdk" do
+  version "21,1"
+
+  if Hardware::CPU.intel?
+    url "https://github.com/SAP/SapMachine/releases/download/sapmachine-#{version.before_comma}%2B#{version.after_comma}/sapmachine-jdk-#{version.before_comma}-ea.#{version.after_comma}_macos-x64_bin.dmg",
+         verified: "https://github.com/SAP/SapMachine"
+    sha256 "73452cad0b2464359e0e88c9cc51251618a6bc0de23395c57542892c40588562"
+  else
+    url "https://github.com/SAP/SapMachine/releases/download/sapmachine-#{version.before_comma}%2B#{version.after_comma}/sapmachine-jdk-#{version.before_comma}-ea.#{version.after_comma}_macos-aarch64_bin.dmg",
+         verified: "https://github.com/SAP/SapMachine"
+    sha256 "d29e960aa80564253eb7645ce4707a36f0289eb05e829e42fe5404746c36056e"
+  end
+
+  appcast "https://sap.github.io/SapMachine/latest/#{version.major}"
+  name "SapMachine OpenJDK Development Kit"
+  desc "OpenJDK build from SAP"
+  homepage "https://sapmachine.io/"
+
+  artifact "sapmachine-jdk-#{version.before_comma}.jdk", target: "/Library/Java/JavaVirtualMachines/sapmachine-#{version.major}-ea.jdk"
+
+  uninstall rmdir: "/Library/Java/JavaVirtualMachines"
+end
