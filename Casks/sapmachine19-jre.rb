3fb2f26d3b7019f208e7402eee30adca657c2e14
SapMachine <sapmachine@sap.com>
2023-01-19 10:20:55 +0100

    Update sapmachine19-jre.rb (19.0.2).

--- Casks/sapmachine19-jre.rb
+++ Casks/sapmachine19-jre.rb
@@ -1,15 +1,15 @@
 
 cask "sapmachine19-jre" do
-  version "19.0.1"
+  version "19.0.2"
 
   if Hardware::CPU.intel?
     url "https://github.com/SAP/SapMachine/releases/download/sapmachine-#{version}/sapmachine-jre-#{version}_macos-x64_bin.dmg",
          verified: "https://github.com/SAP/SapMachine"
-    sha256 "402893bb8147b83b12e7ebf2168a113db0e538bb9b08bac02a1b6543d8baaa0f"
+    sha256 "09ae2fd8bcbf3f1e458f63df28398679df30c61e297c122ff998855d3bc37f33"
   else
     url "https://github.com/SAP/SapMachine/releases/download/sapmachine-#{version}/sapmachine-jre-#{version}_macos-aarch64_bin.dmg",
          verified: "https://github.com/SAP/SapMachine"
-    sha256 "cbbc6455fc17edbd348be20ed1945330196383155486e5aa71f00aa1cc762304"
+    sha256 "99dfb9a5e9614e1d8ffde8cab718e4a03bd6e822bdb77652f3348ba0fc9d4c69"
   end
 
   appcast "https://sap.github.io/SapMachine/latest/#{version.major}"
