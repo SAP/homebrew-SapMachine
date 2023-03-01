5c76434cdad07412f2bf4bcec286f5659418206b
SapMachine <sapmachine@sap.com>
2023-03-01 01:54:41 +0100

    Update sapmachine17-ea-jre.rb (17.0.7+3).

--- Casks/sapmachine17-ea-jre.rb
+++ Casks/sapmachine17-ea-jre.rb
@@ -1,15 +1,15 @@
 
 cask "sapmachine17-ea-jre" do
-  version "17.0.7,2"
+  version "17.0.7,3"
 
   if Hardware::CPU.intel?
     url "https://github.com/SAP/SapMachine/releases/download/sapmachine-#{version.before_comma}%2B#{version.after_comma}/sapmachine-jre-#{version.before_comma}-ea.#{version.after_comma}_macos-x64_bin.dmg",
          verified: "https://github.com/SAP/SapMachine"
-    sha256 "b8ebd7952e9ecf2d47b8696bca3c150d1b997418cb04b63700795d1aac6cc1d6"
+    sha256 "540f622558477417738ee7842e06413ef9c58c09643ff55027dee8c37ebe7469"
   else
     url "https://github.com/SAP/SapMachine/releases/download/sapmachine-#{version.before_comma}%2B#{version.after_comma}/sapmachine-jre-#{version.before_comma}-ea.#{version.after_comma}_macos-aarch64_bin.dmg",
          verified: "https://github.com/SAP/SapMachine"
-    sha256 "a6ffd11bd304877795a0cad409aaadf31b0071e98a34484238bc39051f76a5d6"
+    sha256 "9fa537e2dd602ddc654fe27ed10b1f8152309103133a05c7b7eebb89a4e62916"
   end
 
   appcast "https://sap.github.io/SapMachine/latest/#{version.major}"
