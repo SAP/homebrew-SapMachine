44592ed2c1b42af1927e4a242027feb5d7ddaa20
SapMachine <sapmachine@sap.com>
2023-07-10 11:08:44 +0200

    Update sapmachine11-jdk.rb (11.0.19).

--- Casks/sapmachine11-jdk.rb
+++ Casks/sapmachine11-jdk.rb
@@ -1,23 +1,22 @@
-
 cask "sapmachine11-jdk" do
   version "11.0.19"
+  arch arm: "aarch64", intel: "x64"
+  sha256 arm:   "90876d2396dece12bab4f1058a45252db371ace05fc4c08ac7b14a3f74115d19",
+         intel: "47fa3722ee0b4fa65a95607719743ddee45e115ac624ceec9dbe9739e044a70a"
 
-  if Hardware::CPU.intel?
-    url "https://github.com/SAP/SapMachine/releases/download/sapmachine-#{version}/sapmachine-jdk-#{version}_macos-x64_bin.dmg",
-         verified: "https://github.com/SAP/SapMachine"
-    sha256 "47fa3722ee0b4fa65a95607719743ddee45e115ac624ceec9dbe9739e044a70a"
-  else
-    url "https://github.com/SAP/SapMachine/releases/download/sapmachine-#{version}/sapmachine-jdk-#{version}_macos-aarch64_bin.dmg",
-         verified: "https://github.com/SAP/SapMachine"
-    sha256 "90876d2396dece12bab4f1058a45252db371ace05fc4c08ac7b14a3f74115d19"
-  end
+  url "https://github.com/SAP/SapMachine/releases/download/sapmachine-#{version}/sapmachine-jdk-#{version}_macos-#{arch}_bin.dmg",
+      verified: "github.com/SAP/SapMachine/"
 
-  appcast "https://sap.github.io/SapMachine/latest/#{version.major}"
   name "SapMachine OpenJDK Development Kit"
-  desc "OpenJDK build from SAP"
+  desc "OpenJDK distribution from SAP"
   homepage "https://sapmachine.io/"
 
-  artifact "sapmachine-jdk-#{version}.jdk", target: "/Library/Java/JavaVirtualMachines/sapmachine-#{version.major}.jdk"
+  # The version information on the homepage is rendered client-side from the
+  # following JSON file, so we have to check it instead.
+  livecheck do
+    url "https://sap.github.io/SapMachine/assets/data/sapmachine_releases.json"
+    regex(/["']tag["']:\s*["']sapmachine[._-]v?(\d+(?:\.\d+)*)["']/i)
+  end
 
-  uninstall rmdir: "/Library/Java/JavaVirtualMachines"
+  artifact "sapmachine-jdk-#{version}.jdk", target: "/Library/Java/JavaVirtualMachines/sapmachine-#{version.major}.jdk"
 end
