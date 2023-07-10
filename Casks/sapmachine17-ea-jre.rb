29252790ee12ad022e57256302f5808adf877b2c
SapMachine <sapmachine@sap.com>
2023-07-10 11:09:50 +0200

    Update sapmachine17-ea-jre.rb (17.0.8+6).

--- Casks/sapmachine17-ea-jre.rb
+++ Casks/sapmachine17-ea-jre.rb
@@ -1,23 +1,22 @@
-
 cask "sapmachine17-ea-jre" do
   version "17.0.8,6"
+  arch arm: "aarch64", intel: "x64"
+  sha256 arm:   "b319471ec2fef9fd7dfd73b1378fa6eef37b7ac1729f1c404a351804ac1aeaf9",
+         intel: "4fd18d9580bf9bddd5c3ce7d49ac5c5b5965071fa15a86f187d76c4f9a082681"
 
-  if Hardware::CPU.intel?
-    url "https://github.com/SAP/SapMachine/releases/download/sapmachine-#{version.before_comma}%2B#{version.after_comma}/sapmachine-jre-#{version.before_comma}-ea.#{version.after_comma}_macos-x64_bin.dmg",
-         verified: "https://github.com/SAP/SapMachine"
-    sha256 "4fd18d9580bf9bddd5c3ce7d49ac5c5b5965071fa15a86f187d76c4f9a082681"
-  else
-    url "https://github.com/SAP/SapMachine/releases/download/sapmachine-#{version.before_comma}%2B#{version.after_comma}/sapmachine-jre-#{version.before_comma}-ea.#{version.after_comma}_macos-aarch64_bin.dmg",
-         verified: "https://github.com/SAP/SapMachine"
-    sha256 "b319471ec2fef9fd7dfd73b1378fa6eef37b7ac1729f1c404a351804ac1aeaf9"
-  end
+  url "https://github.com/SAP/SapMachine/releases/download/sapmachine-#{version.before_comma}%2B#{version.after_comma}/sapmachine-jre-#{version.before_comma}-ea.#{version.after_comma}_macos-#{arch}_bin.dmg",
+      verified: "github.com/SAP/SapMachine/"
 
-  appcast "https://sap.github.io/SapMachine/latest/#{version.major}"
   name "SapMachine OpenJDK Development Kit"
-  desc "OpenJDK build from SAP"
+  desc "OpenJDK distribution from SAP"
   homepage "https://sapmachine.io/"
 
-  artifact "sapmachine-jre-#{version.before_comma}.jre", target: "/Library/Java/JavaVirtualMachines/sapmachine-#{version.major}-ea.jre"
+  # The version information on the homepage is rendered client-side from the
+  # following JSON file, so we have to check it instead.
+  livecheck do
+    url "https://sap.github.io/SapMachine/assets/data/sapmachine_releases.json"
+    regex(/["']tag["']:\s*["']sapmachine[._-]v?(\d+(?:\.\d+)*)["']/i)
+  end
 
-  uninstall rmdir: "/Library/Java/JavaVirtualMachines"
+  artifact "sapmachine-jre-#{version.before_comma}.jre", target: "/Library/Java/JavaVirtualMachines/sapmachine-#{version.major}-ea.jre"
 end
