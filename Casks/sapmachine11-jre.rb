8ceabc43296ba0954527fcc437273fd49172d81f
SapMachine <sapmachine@sap.com>
2023-07-10 11:08:44 +0200

    Update sapmachine11-jre.rb (11.0.19).

--- Casks/sapmachine11-jre.rb
+++ Casks/sapmachine11-jre.rb
@@ -1,23 +1,22 @@
-
 cask "sapmachine11-jre" do
   version "11.0.19"
+  arch arm: "aarch64", intel: "x64"
+  sha256 arm:   "135d11caa3d2199fa037cd10341f8f3cd068e583b74ac528902d072822ef400c",
+         intel: "c00b4b6660beabe986e4149e79768b9f3164353eee968723a28d8c5c6618fe75"
 
-  if Hardware::CPU.intel?
-    url "https://github.com/SAP/SapMachine/releases/download/sapmachine-#{version}/sapmachine-jre-#{version}_macos-x64_bin.dmg",
-         verified: "https://github.com/SAP/SapMachine"
-    sha256 "c00b4b6660beabe986e4149e79768b9f3164353eee968723a28d8c5c6618fe75"
-  else
-    url "https://github.com/SAP/SapMachine/releases/download/sapmachine-#{version}/sapmachine-jre-#{version}_macos-aarch64_bin.dmg",
-         verified: "https://github.com/SAP/SapMachine"
-    sha256 "135d11caa3d2199fa037cd10341f8f3cd068e583b74ac528902d072822ef400c"
-  end
+  url "https://github.com/SAP/SapMachine/releases/download/sapmachine-#{version}/sapmachine-jre-#{version}_macos-#{arch}_bin.dmg",
+      verified: "github.com/SAP/SapMachine/"
 
-  appcast "https://sap.github.io/SapMachine/latest/#{version.major}"
   name "SapMachine OpenJDK Development Kit"
-  desc "OpenJDK build from SAP"
+  desc "OpenJDK distribution from SAP"
   homepage "https://sapmachine.io/"
 
-  artifact "sapmachine-jre-#{version}.jre", target: "/Library/Java/JavaVirtualMachines/sapmachine-#{version.major}.jre"
+  # The version information on the homepage is rendered client-side from the
+  # following JSON file, so we have to check it instead.
+  livecheck do
+    url "https://sap.github.io/SapMachine/assets/data/sapmachine_releases.json"
+    regex(/["']tag["']:\s*["']sapmachine[._-]v?(\d+(?:\.\d+)*)["']/i)
+  end
 
-  uninstall rmdir: "/Library/Java/JavaVirtualMachines"
+  artifact "sapmachine-jre-#{version}.jre", target: "/Library/Java/JavaVirtualMachines/sapmachine-#{version.major}.jre"
 end
