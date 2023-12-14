9a986ae05f8b015102e79cbf20d067ba4ffa4bb0
SapMachine <sapmachine@sap.com>
2023-12-14 01:54:59 +0100

    Update sapmachine23-ea-jdk.rb (23+1).

---
+++ Casks/sapmachine23-ea-jdk.rb
@@ -0,0 +1,22 @@
+cask "sapmachine23-ea-jdk" do
+  version "23,1"
+  arch arm: "aarch64", intel: "x64"
+  sha256 arm:   "542ee75d4b457dce2639ba9dbe4529e0834af375399bb3b86ac5908f2dca1bd8",
+         intel: "10bc47c919751fb984a89e128ef74ea33e047d1a047fa0def09e2b423e42064f"
+
+  url "https://github.com/SAP/SapMachine/releases/download/sapmachine-#{version.before_comma}%2B#{version.after_comma}/sapmachine-jdk-#{version.before_comma}-ea.#{version.after_comma}_macos-#{arch}_bin.dmg",
+      verified: "github.com/SAP/SapMachine/"
+
+  name "SapMachine OpenJDK Development Kit"
+  desc "OpenJDK distribution from SAP"
+  homepage "https://sapmachine.io/"
+
+  # The version information on the homepage is rendered client-side from the
+  # following JSON file, so we have to check it instead.
+  livecheck do
+    url "https://sap.github.io/SapMachine/assets/data/sapmachine_releases.json"
+    regex(/["']tag["']:\s*["']sapmachine[._-]v?(\d+(?:\.\d+)*)["']/i)
+  end
+
+  artifact "sapmachine-jdk-#{version.before_comma}.jdk", target: "/Library/Java/JavaVirtualMachines/sapmachine-#{version.major}-ea.jdk"
+end
