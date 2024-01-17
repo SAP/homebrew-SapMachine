5f63911e363b87a4c200bb74044cd3988fc3e1bb
Christoph Langer <christoph.langer@sap.com>
2024-01-17 18:02:05 +0100

    Remove appcast stanza in sapmachine12-jdk.rb

--- Casks/sapmachine12-jdk.rb
+++ Casks/sapmachine12-jdk.rb
@@ -4,7 +4,6 @@ cask 'sapmachine12-jdk' do
   sha256 'ede180009ded8bef782e41ad72bd242faa6749ac889297c01777559278fc4848'
 
   url "https://github.com/SAP/SapMachine/releases/download/sapmachine-#{version}/sapmachine-jdk-#{version}_osx-x64_bin.tar.gz"
-  appcast "https://sap.github.io/SapMachine/latest/#{version.major}"
   name 'SapMachine OpenJDK Development Kit'
   homepage 'https://sapmachine.io/'
 
