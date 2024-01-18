dcac0de952b78e06c21fbad3d4ab7b312c3bc87b
Christoph Langer <christoph.langer@sap.com>
2024-01-18 17:19:29 +0100

    Try new livecheck URL with sapmachine23-ea-jdk

--- Casks/sapmachine23-ea-jdk.rb
+++ Casks/sapmachine23-ea-jdk.rb
@@ -14,7 +14,7 @@ cask "sapmachine23-ea-jdk" do
   # The version information on the homepage is rendered client-side from the
   # following JSON file, so we have to check it instead.
   livecheck do
-    url "https://sap.github.io/SapMachine/assets/data/sapmachine_releases.json"
+    url "https://sap.github.io/SapMachine/assets/data/sapmachine-releases-#{version.major}.json"
     regex(/["']tag["']:\s*["']sapmachine[._-]v?(\d+(?:\.\d+)*)["']/i)
   end
 
