954e78b252b7908157f13583fa91b666ca21ca1b
SapMachine <sapmachine@sap.com>
2024-01-20 16:24:54 +0100

    Update sapmachine22-ea-jdk.rb (22+32).

--- Casks/sapmachine22-ea-jdk.rb
+++ Casks/sapmachine22-ea-jdk.rb
@@ -1,8 +1,8 @@
 cask "sapmachine22-ea-jdk" do
-  version "22,31"
+  version "22,32"
   arch arm: "aarch64", intel: "x64"
-  sha256 arm:   "41793e58f7bbd4beec0935ce017dcc726e8feb4412aa0f54e83463686ef41cf9",
-         intel: "c56b52155a8b8cd3d8b00661b9561fd026baed2ee7a616de9ae34b97626904e2"
+  sha256 arm:   "ae95a7035d19c95e80bdbe09b3aebb5930db8be675133ee6780ec4d14997c665",
+         intel: "0b2d9ac36cd834e4f16926c9c88e5168bb7039674abf97aa2d1d3375bfac4e6b"
 
   url "https://github.com/SAP/SapMachine/releases/download/sapmachine-#{version.before_comma}%2B#{version.after_comma}/sapmachine-jdk-#{version.before_comma}-ea.#{version.after_comma}_macos-#{arch}_bin.dmg",
       verified: "github.com/SAP/SapMachine/"
@@ -11,10 +11,9 @@ cask "sapmachine22-ea-jdk" do
   desc "OpenJDK distribution from SAP"
   homepage "https://sapmachine.io/"
 
-  # The version information on the homepage is rendered client-side from the
-  # following JSON file, so we have to check it instead.
+  # Check for latest version in SapMachine release data.
   livecheck do
-    url "https://sap.github.io/SapMachine/assets/data/sapmachine_releases.json"
+    url "https://sap.github.io/SapMachine/assets/data/sapmachine-releases-latest.json"
     regex(/["']tag["']:\s*["']sapmachine[._-]v?(\d+(?:\.\d+)*)["']/i)
   end
 
