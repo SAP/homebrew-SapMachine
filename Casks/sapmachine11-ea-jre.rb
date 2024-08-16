4aabb66f92e8fd09dcedbb029eb3d784bb87afff
SapMachine <sapmachine@sap.com>
2024-08-16 04:40:10 +0200

    Update sapmachine11-ea-jre.rb (11.0.25+3).

--- Casks/sapmachine11-ea-jre.rb
+++ Casks/sapmachine11-ea-jre.rb
@@ -1,8 +1,8 @@
 cask "sapmachine11-ea-jre" do
-  version "11.0.25,2"
+  version "11.0.25,3"
   arch arm: "aarch64", intel: "x64"
-  sha256 arm:   "fcc6a7f1bc290b7cba59dc73d970ce6c609a97bfdaa4c44e7a5cb93cd5438b23",
-         intel: "dec5fb2502c3bf8315cc450e5366f23d469e310edef7ce5130dba28952a94714"
+  sha256 arm:   "8b0a9bb0d17a7aa66b8f7c954f80c77127b7d072a47c78af591d07ac8951f990",
+         intel: "3a077a9aae8f9bdc5eefeab8c9d5270f801027bbccfc48ca193bfaca96d608cc"
 
   url "https://github.com/SAP/SapMachine/releases/download/sapmachine-#{version.before_comma}%2B#{version.after_comma}/sapmachine-jre-#{version.before_comma}-ea.#{version.after_comma}_macos-#{arch}_bin.dmg",
       verified: "github.com/SAP/SapMachine/"
