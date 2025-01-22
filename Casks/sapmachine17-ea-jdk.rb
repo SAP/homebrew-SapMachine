07f766bf809b5289db683f9610fd9b6f2a3f816a
SapMachine <sapmachine@sap.com>
2025-01-22 04:10:17 +0100

    Update sapmachine17-ea-jdk.rb (17.0.14+7).

--- Casks/sapmachine17-ea-jdk.rb
+++ Casks/sapmachine17-ea-jdk.rb
@@ -1,8 +1,8 @@
 cask "sapmachine17-ea-jdk" do
-  version "17.0.14,6"
+  version "17.0.14,7"
   arch arm: "aarch64", intel: "x64"
-  sha256 arm:   "c16e0e6b4babbaaa445572f81e83e651ace7fff78da39ec6a8427d0748c50ef2",
-         intel: "c00821138c633a67fda84d62441e944be9369e743434efababc99df2c3b4a46b"
+  sha256 arm:   "ee1511cff52f0c0a7091dd4c95b4a09071a388807e14d136730ce4c750f5ddd6",
+         intel: "63456374819093664a04d53429a9ff4c57c7a158c3cc7bea11289319be483a4e"
 
   url "https://github.com/SAP/SapMachine/releases/download/sapmachine-#{version.before_comma}%2B#{version.after_comma}/sapmachine-jdk-#{version.before_comma}-ea.#{version.after_comma}_macos-#{arch}_bin.dmg",
       verified: "github.com/SAP/SapMachine/"
