d02edec6f5461efa23809568524ac7a1d4eaf12f
SapMachine <sapmachine@sap.com>
2025-03-27 13:11:55 +0100

    Update sapmachine25-ea-jdk.rb (25+15).

--- Casks/sapmachine25-ea-jdk.rb
+++ Casks/sapmachine25-ea-jdk.rb
@@ -1,8 +1,8 @@
 cask "sapmachine25-ea-jdk" do
-  version "25,14"
+  version "25,15"
   arch arm: "aarch64", intel: "x64"
-  sha256 arm:   "e8872c070501b01907798c105c12ddb28289ba3210a7bb2284294d8a83b49112",
-         intel: "eec4b8b632ca29516659eef7f8b1e00933ac548775f7ba26d4c8a959f87401fb"
+  sha256 arm:   "c1e5d1a45008603be1e8c1ac22463b4363130667426b0d1f50c4892b72f25d76",
+         intel: "1801dd92e3fd629b0e746bb4749ab463512dd8ecca655ae501c202fc44bac47a"
 
   url "https://github.com/SAP/SapMachine/releases/download/sapmachine-#{version.before_comma}%2B#{version.after_comma}/sapmachine-jdk-#{version.before_comma}-ea.#{version.after_comma}_macos-#{arch}_bin.dmg",
       verified: "github.com/SAP/SapMachine/"
