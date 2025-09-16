5801b6cbc0e9b0876cf11e2375eb16081be82cc8
SapMachine <sapmachine@sap.com>
2025-09-16 07:30:26 +0200

    Update sapmachine26-ea-jre.rb (26+15).

--- Casks/sapmachine26-ea-jre.rb
+++ Casks/sapmachine26-ea-jre.rb
@@ -1,10 +1,8 @@
 cask "sapmachine26-ea-jre" do
-  version "26,14"
-  arch arm: "aarch64", intel: "x64"
-  sha256 arm:   "c11dbfc9dfef08ca16a16d1462579f63bfa8f48637760179748913e3af2547d0",
-         intel: "9fb1486a04b4e999bd49ec5271330e0f90dfd9dee6f7195a96ad2ead26bc529c"
+  version "26,15"
+  sha256 "6c51e43f81cd711bcb36bb030b0d1ddd13224ffaf80c7a5826d9b4418f57c14b"
 
-  url "https://github.com/SAP/SapMachine/releases/download/sapmachine-#{version.before_comma}%2B#{version.after_comma}/sapmachine-jre-#{version.before_comma}-ea.#{version.after_comma}_macos-#{arch}_bin.dmg",
+  url "https://github.com/SAP/SapMachine/releases/download/sapmachine-#{version.before_comma}%2B#{version.after_comma}/sapmachine-jre-#{version.before_comma}-ea.#{version.after_comma}_macos-aarch64_bin.dmg",
       verified: "github.com/SAP/SapMachine/"
 
   name "SapMachine OpenJDK Development Kit"
