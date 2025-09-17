578d2492da315a6f7e2fc2b105c98ccd12bbe28d
SapMachine <sapmachine@sap.com>
2025-09-17 14:37:49 +0200

    Update sapmachine25-jre.rb (25).

--- Casks/sapmachine25-jre.rb
+++ Casks/sapmachine25-jre.rb
@@ -2,8 +2,10 @@ cask "sapmachine25-jre" do
   version "25"
   sha256 "c0c69ffb7838ec46ce2b4f7bf88598087731ddce692ec426d02ca13e881cc37d"
 
-  url "https://github.com/SAP/SapMachine/releases/download/sapmachine-#{version}/sapmachine-jre-#{version}_macos-aarch64_bin.dmg",
-      verified: "github.com/SAP/SapMachine/"
+  on_arm do
+    url "https://github.com/SAP/SapMachine/releases/download/sapmachine-#{version}/sapmachine-jre-#{version}_macos-aarch64_bin.dmg",
+        verified: "github.com/SAP/SapMachine/"
+  end
 
   name "SapMachine OpenJDK Development Kit"
   desc "OpenJDK distribution from SAP"
