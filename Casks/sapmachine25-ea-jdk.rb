25fb9acfbccc76fd0feffd362279626b9e88df5e
SapMachine <sapmachine@sap.com>
2025-10-01 12:56:28 +0200

    Update sapmachine25-ea-jdk.rb (25.0.1+1).

--- Casks/sapmachine25-ea-jdk.rb
+++ Casks/sapmachine25-ea-jdk.rb
@@ -1,10 +1,10 @@
 cask "sapmachine25-ea-jdk" do
-  version "25,36"
-  arch arm: "aarch64", intel: "x64"
-  sha256 arm:   "83f19e57af7b32183990ed86a78f41fc4f3ff680dbab922344747d23d9eeae4b",
-         intel: "cf7d45b86658b8cc34c7d286b8baf236b1f399513bf109c9ab0495f81f9e5902"
+  version "25.0.1,1"
+  sha256 "b7024b28b8d3c73614960251cb6b34b825801839792f16306f89e90aef5a9bf3"
 
-  url "https://github.com/SAP/SapMachine/releases/download/sapmachine-#{version.before_comma}%2B#{version.after_comma}/sapmachine-jdk-#{version.before_comma}-ea.#{version.after_comma}_macos-#{arch}_bin.dmg",
+  depends_on arch: :arm64
+
+  url "https://github.com/SAP/SapMachine/releases/download/sapmachine-#{version.before_comma}%2B#{version.after_comma}/sapmachine-jdk-#{version.before_comma}-ea.#{version.after_comma}_macos-aarch64_bin.dmg",
       verified: "github.com/SAP/SapMachine/"
 
   name "SapMachine OpenJDK Development Kit"
