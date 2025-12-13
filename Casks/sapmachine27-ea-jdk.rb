bfc908a4b42f9ed2d20df09ea175f0c86faf7daa
SapMachine <sapmachine@sap.com>
2025-12-13 02:53:10 +0100

    Update sapmachine27-ea-jdk.rb (27+1).

---
+++ Casks/sapmachine27-ea-jdk.rb
@@ -0,0 +1,21 @@
+cask "sapmachine27-ea-jdk" do
+  version "27,1"
+  sha256 "19e1b13ded2dc6bfad5e5129cad9b18c0ec6cd28ebc4625225b5205da92fe527"
+
+  depends_on arch: :arm64
+
+  url "https://github.com/SAP/SapMachine/releases/download/sapmachine-#{version.before_comma}%2B#{version.after_comma}/sapmachine-jdk-#{version.before_comma}-ea.#{version.after_comma}_macos-aarch64_bin.dmg",
+      verified: "github.com/SAP/SapMachine/"
+
+  name "SapMachine OpenJDK Development Kit"
+  desc "OpenJDK distribution from SAP"
+  homepage "https://sapmachine.io/"
+
+  # Check for latest version in SapMachine release data.
+  livecheck do
+    url "https://sap.github.io/SapMachine/assets/data/sapmachine-releases-latest.json"
+    regex(/["']tag["']:\s*["']sapmachine[._-]v?(\d+(?:\.\d+)*)["']/i)
+  end
+
+  artifact "sapmachine-jdk-#{version.before_comma}.jdk", target: "/Library/Java/JavaVirtualMachines/sapmachine-#{version.major}-ea.jdk"
+end
