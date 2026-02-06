b5339a913ff53314de1031c0098172b2f1b932f4
SapMachine <sapmachine@sap.com>
2026-02-06 02:38:43 +0100

    Update sapmachine26-ea-jdk.rb (26+33).

--- Casks/sapmachine26-ea-jdk.rb
+++ Casks/sapmachine26-ea-jdk.rb
@@ -1,6 +1,6 @@
 cask "sapmachine26-ea-jdk" do
-  version "26,32"
-  sha256 "d40dc102948ae8ffca40ef6684c28da02219008ed033347645be3a6df1902e6e"
+  version "26,33"
+  sha256 "6e9054c9f4038fbb9d68b80d6b19b7e8e3e688bf35f10445173702cfebd1b406"
 
   depends_on arch: :arm64
 
