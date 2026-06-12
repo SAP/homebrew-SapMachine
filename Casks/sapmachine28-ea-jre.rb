ab5d56b408eadadad631f476928dcb062c4520d1
SapMachine <sapmachine@sap.com>
2026-06-12 03:23:18 +0200

    Update sapmachine28-ea-jre.rb (28+1).

---
+++ Casks/sapmachine28-ea-jre.rb
@@ -0,0 +1,21 @@
+cask "sapmachine28-ea-jre" do
+  version "28,1"
+  sha256 "fd166c93833dd4afe5286a51819872a9f9874a528652f0e0aa75001ccbd8fde7"
+
+  depends_on arch: :arm64
+
+  url "https://github.com/SAP/SapMachine/releases/download/sapmachine-#{version.before_comma}%2B#{version.after_comma}/sapmachine-jre-#{version.before_comma}-ea.#{version.after_comma}_macos-aarch64_bin.dmg",
+      verified: "github.com/SAP/SapMachine/"
+
+  name "SapMachine OpenJDK Development Kit"
+  desc "OpenJDK distribution from SAP"
+  homepage "https://sapmachine.io/"
+
+  # Check for latest version in SapMachine release data.
+  livecheck do
+    url "https://sap.github.io/SapMachine/assets/data/sapmachine-releases-latest.json"
+    regex(/["']tag["']:\s*["']sapmachine[._-]v?(\d+(?:\.\d+)*)["']/i)
+  end
+
+  artifact "sapmachine-jre-#{version.before_comma}.jre", target: "/Library/Java/JavaVirtualMachines/sapmachine-#{version.major}-ea.jre"
+end
