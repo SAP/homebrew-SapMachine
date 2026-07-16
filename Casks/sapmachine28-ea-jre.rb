4ae75393839a183ea660c09ad49770fda7f9477e
SapMachine <sapmachine@sap.com>
2026-07-16 17:25:14 +0200

    Update sapmachine28-ea-jre.rb (28+6).

--- Casks/sapmachine28-ea-jre.rb
+++ Casks/sapmachine28-ea-jre.rb
@@ -1,6 +1,6 @@
 cask "sapmachine28-ea-jre" do
-  version "28,5"
-  sha256 "a469efb89cdc622cd371cd91c7e3e5df141e7ca1b23fab78f02eca5950cac795"
+  version "28,6"
+  sha256 "934874b3650623e7739515edcda983be74e2ab484df2c928ab36f3a5ccd1e025"
 
   depends_on arch: :arm64
 
